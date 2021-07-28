34955e619979d3857d1a515c899b9809450abb90
Jeremy Rose <nornagon@nornagon.net>
2021-07-28 09:19:08 -0700

    allow loading as a library

--- index.js
+++ index.js
@@ -3,22 +3,32 @@
 const protobuf = require('protobufjs')
 const https = require('https')
 
-protobuf.load(__dirname + '/variations_seed.proto', (err, root) => {
-  if (err) throw err
-  const VariationsSeed = root.lookupType('variations.VariationsSeed')
-  console.error('fetching variations data...')
-  const req = https.request('https://clientservices.googleapis.com/chrome-variations/seed')
-  req.on('response', res => {
-    let buf = Buffer.alloc(0)
-    res.on('data', (data) => {
-      buf = Buffer.concat([buf, data])
-    })
-    res.on('end', () => {
-      console.error('fetched variations data, parsing...')
-      const seed = VariationsSeed.decode(buf)
-      //console.log(require('util').inspect(seed, {depth: Infinity}))
-      console.log(JSON.stringify(seed, null, 2))
+function fetchVariations() {
+  return new Promise((resolve, reject) => {
+    protobuf.load(__dirname + '/variations_seed.proto', (err, root) => {
+      if (err) return reject(err)
+      const VariationsSeed = root.lookupType('variations.VariationsSeed')
+      const req = https.request('https://clientservices.googleapis.com/chrome-variations/seed')
+      req.on('response', res => {
+        let buf = Buffer.alloc(0)
+        res.on('data', (data) => {
+          buf = Buffer.concat([buf, data])
+        })
+        res.on('end', () => {
+          const seed = VariationsSeed.decode(buf)
+          resolve(seed)
+        })
+      })
+      req.on('error', (err) => reject(err))
+      req.end()
     })
   })
-  req.end()
-})
+}
+
+module.exports = fetchVariations
+
+if (!module.parent) {
+  fetchVariations().then(seed => {
+    console.log(JSON.stringify(seed, null, 2))
+  })
+}
